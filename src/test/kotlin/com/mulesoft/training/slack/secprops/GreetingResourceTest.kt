2eec697cabfda061b9a3dd4b86abfe1eacbc9eb5
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-02-17 13:16:41 +0100

    irrelevant syntax change

--- src/test/kotlin/com/mulesoft/training/slack/secprops/GreetingResourceTest.kt
+++ src/test/kotlin/com/mulesoft/training/slack/secprops/GreetingResourceTest.kt
@@ -9,11 +9,9 @@ import org.junit.jupiter.api.Test
 class GreetingResourceTest {
 
     @Test
-    fun testHelloEndpoint() {
-        given()
-            .`when`().get("/hello")
-            .then()
-            .statusCode(200)
-            .body(`is`("Hello RESTEasy"))
-    }
+    fun testHelloEndpoint() = given()
+        .`when`().get("/hello")
+        .then()
+        .statusCode(200)
+        .body(`is`("Hello RESTEasy"))
 }
