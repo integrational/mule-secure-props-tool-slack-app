8514b6e330e1f7e0183c20e2a4a9fef48c0ff45b
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-03-04 14:44:47 +0100

    slight refactoring

--- src/main/kotlin/com/mulesoft/training/slack/secprops/rest/AboutResource.kt
+++ src/main/kotlin/com/mulesoft/training/slack/secprops/rest/AboutResource.kt
@@ -1,6 +1,7 @@
 package com.mulesoft.training.slack.secprops.rest
 
 import com.mulesoft.training.slack.secprops.config.QuarkusApp
+import javax.enterprise.context.ApplicationScoped
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -8,10 +9,13 @@ import javax.ws.rs.core.MediaType
 
 @Path("/about")
 @Produces(MediaType.APPLICATION_JSON)
-class AboutResource(val quarkusApp: QuarkusApp) {
-
-    @GET
-    fun about() = About(quarkusApp.name(), quarkusApp.version())
+@ApplicationScoped
+class AboutResource(thisApp: QuarkusApp) {
 
     data class About(val app: String, val version: String)
+
+    private val about = About(thisApp.name(), thisApp.version())
+
+    @GET
+    fun about() = about
 }
