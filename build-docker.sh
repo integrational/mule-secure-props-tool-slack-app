1d32bc214f2bb5dba5248f00bc98d0710402ebcf
Gerald Loeffler <gerald.loeffler@googlemail.com>
2022-03-04 08:17:06 +0100

    using latest base image and always pulling on build

--- build-docker.sh
+++ build-docker.sh
@@ -4,7 +4,8 @@ cd $(cd "$(dirname "$0")" && pwd) # cd to where this script is located
 
 # native build doesn't work once Slack is added
 #./gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true
-#docker build -f src/main/docker/Dockerfile.native-micro -t $IMG .
+#docker build --pull -f src/main/docker/Dockerfile.native-micro -t $IMG .
+
 # normal Java-based Docker image instead
 ./gradlew build -Dquarkus.package.type=uber-jar
-docker build -f src/main/docker/Dockerfile.jvm -t $IMG .
+docker build --pull -f src/main/docker/Dockerfile.jvm -t $IMG .
