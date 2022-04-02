824becdb2fffd24227126ffb1a922f61aeb9a9c5
GeraldLoeffler <gerald.loeffler@googlemail.com>
2022-04-02 12:10:26 +0200

    added Heroku prod URL

--- ping.sh
+++ ping.sh
@@ -2,9 +2,14 @@
 set -Eeuo pipefail
 cd $(cd "$(dirname "$0")" && pwd) # cd to where this script is located
 
-host=localhost
-port=3000
-base="http://$host:$port"
+# local dev
+# host=localhost
+# port=3000
+# base="http://$host:$port"
+
+# Heroku prod
+host=mule-secure-props-tool-slack.herokuapp.com
+base="https://$host"
 
 # according to MicroProfile Health
 curl -i $base/q/health/started
